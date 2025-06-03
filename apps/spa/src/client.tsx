dcabba96e2e03000aaa8a1612537a262a6d357e6
Alexandru Ţurcanu <pondorasti@gmail.com>
2025-06-04 04:37:39 +0800

    feat: turborepo

--- apps/spa/src/client.tsx
+++ apps/spa/src/client.tsx
@@ -1,8 +1,8 @@
 // src/client.tsx
 /// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
-import { createRouter } from './router'
+import { hydrateRoot } from "react-dom/client"
+import { StartClient } from "@tanstack/react-start"
+import { createRouter } from "./router"
 
 const router = createRouter()
 
